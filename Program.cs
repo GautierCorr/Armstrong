99c91feaf7741e7974db2b853c5c1cd5bf362ce2
Gautier_Correur <gautier.correur@etud.u-picardie.fr>
2022-05-03 17:06:56 +0200

    Création sous programme Armstrong

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Armstrong
+{
+    class Program
+    {
+	static bool Armstrong(int nombre)
+	{
+		int centaine,dizaine,unite,reste;
+		centaine = (nombre / 100);
+		reste = (nombre % 100);
+		dizaine = (reste / 10);
+		unite = reste % 10;
+		
+		if (centaine*centaine*centaine + dizaine*dizaine*dizaine + unite*unite*unite == nombre)
+		{
+			return true;
+		} else 
+			return false;
+	}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Armstrong(153));
+        }
+    }
+}
